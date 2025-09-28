61f5ad1e30b062109bf26e6c62f18b7d795d938b
Zairion Palmer <zairionpalmer@gmail.com>
2025-09-28 02:00:53 -0400

    feat: sidebarpr2 ready+functional

--- webapp/webapp/services/loginService.js
+++ webapp/webapp/services/loginService.js
@@ -1,6 +1,7 @@
 const apiUrl = import.meta.env.VITE_API_URL;
 
 export async function loginService(email, password){ 
+    console.log(apiUrl);
     return await fetch(`${apiUrl}/auth/login`, {
         method: 'POST',
         headers: {"Content-Type": "application/json"},
